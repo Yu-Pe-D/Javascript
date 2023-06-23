1ef74720ed705b8e2c166d53980f394189d98d8c
Yuri <loccckkki@yandex.ru>
2023-06-23 19:56:14 +0400

    Adding

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
-var name = prompt("Ваше имя?", "");
-        alert("Ваше имя - " + name + "!");
+var nameyours = prompt("Ваше имя?", "");
+        alert("Ваше имя - " + nameyours + "!");
+        var nameyours = prompt("Сколько вам лет?", "");
+let age = 101;
+switch (age) {
+    case 18:
+        alert('Вы совершеннолетний, все можно!');
+        break;
+    case 10:
+        alert('Вам надо учить уроки!');
+        break;
+    case 30:
+        alert('Ложитесь спать, завтра на работу');
+        break;
+    default:
+        alert('Мы не знаем что Вам делать');
+}
